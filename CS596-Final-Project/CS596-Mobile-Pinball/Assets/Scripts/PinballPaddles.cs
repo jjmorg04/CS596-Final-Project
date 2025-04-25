0e7d4bcee05d14046475d97ac0e81364ad6d2bb1
jjmorg04 <jjmorg04@gmail.com>
2025-04-25 13:14:35 -0700

    Added buttons and script.

--- CS596-Final-Project/CS596-Mobile-Pinball/Assets/Scripts/PinballPaddles.cs
+++ CS596-Final-Project/CS596-Mobile-Pinball/Assets/Scripts/PinballPaddles.cs
@@ -1,16 +1,78 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class PinballPaddles : MonoBehaviour
+public class PaddleController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public HingeJoint leftPaddle;
+    public HingeJoint rightPaddle;
+
+    public float paddleForce = 1000f;
+
+    private JointSpring leftSpring;
+    private JointSpring rightSpring;
+
+    public Button leftButton;
+    public Button rightButton;
+
     void Start()
     {
-        
+
+        leftSpring = leftPaddle.spring;
+        rightSpring = rightPaddle.spring;
+
+        leftSpring.spring = paddleForce;
+        leftSpring.damper = 1f;
+
+        rightSpring.spring = paddleForce;
+        rightSpring.damper = 1f;
+
+        if (!leftPaddle || !rightPaddle || !leftButton || !rightButton)
+{
+    Debug.LogError("Missing paddle or button references in Inspector!");
+}
+
+
+        // Attach button event triggers
+        AddEventTrigger(leftButton, EventTriggerType.PointerDown, LeftPaddleDown);
+        AddEventTrigger(leftButton, EventTriggerType.PointerUp, LeftPaddleUp);
+
+        AddEventTrigger(rightButton, EventTriggerType.PointerDown, RightPaddleDown);
+        AddEventTrigger(rightButton, EventTriggerType.PointerUp, RightPaddleUp);
     }
 
-    // Update is called once per frame
-    void Update()
+    void AddEventTrigger(Button button, EventTriggerType eventType, UnityEngine.Events.UnityAction action)
     {
-        
+        EventTrigger trigger = button.gameObject.GetComponent<EventTrigger>();
+        if (trigger == null)
+            trigger = button.gameObject.AddComponent<EventTrigger>();
+
+        EventTrigger.Entry entry = new EventTrigger.Entry { eventID = eventType };
+        entry.callback.AddListener((eventData) => action());
+        trigger.triggers.Add(entry);
+    }
+
+    public void LeftPaddleDown()
+    {
+        leftSpring.targetPosition = -45f;
+        leftPaddle.spring = leftSpring;
+    }
+
+    public void LeftPaddleUp()
+    {
+        leftSpring.targetPosition = 0f;
+        leftPaddle.spring = leftSpring;
+    }
+
+    public void RightPaddleDown()
+    {
+        rightSpring.targetPosition = 45f;
+        rightPaddle.spring = rightSpring;
+    }
+
+    public void RightPaddleUp()
+    {
+        rightSpring.targetPosition = 0f;
+        rightPaddle.spring = rightSpring;
     }
 }
