d305cd44c64125af39bfab3415065d2260bbcc4f
stevengervacio <stevenjgervacio@gmail.com>
2025-05-07 17:39:20 -0700

    Reverse

--- CS596-Final-Project/CS596-Mobile-Pinball/Assets/Scripts/BallCollision.cs
+++ CS596-Final-Project/CS596-Mobile-Pinball/Assets/Scripts/BallCollision.cs
@@ -1,22 +1,12 @@
 using UnityEngine;
 
 public class BallCollision : MonoBehaviour
-public class bvall : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
     {
         if (collision.gameObject.CompareTag("LeftFlipper"))
         {
             ScoreManager.Instance.AddPoint();
         }
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- CS596-Final-Project/CS596-Mobile-Pinball/Assets/Scripts/ScoreManager.cs
+++ CS596-Final-Project/CS596-Mobile-Pinball/Assets/Scripts/ScoreManager.cs
@@ -2,29 +2,21 @@ using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
-public class NewMonoBehaviourScript : MonoBehaviour
 {
     public static ScoreManager instance;
     public TMP_Text Scoreboard;
     private int score = 0;
 
     void Awake()
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
     {
         if (instance == null)
             instance = this;
         else
             Destroy(gameObject);
-        
     }
     public void AddPoint(int points)
-
-    // Update is called once per frame
-    void Update()
     {
         score += points;
         Scoreboard.text = "Score: " + score;
-        
     }
 }
