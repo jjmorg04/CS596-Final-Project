34475e3b21663e9015f2d2d0266d1a367c8ac0bd
JerBearJets <145627532+JerBearJets@users.noreply.github.com>
2025-05-08 11:05:55 -0700

    Added Yellow Point Circles
    - Yellow point circle script
    - Yellow points add up
    - Yellow points make sound
    - Added extra barriers

---
+++ CS596-Final-Project/CS596-Mobile-Pinball/Assets/ScoreZone.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreZone : MonoBehaviour
+{
+    public int scoreValue = 100; // Points awarded
+    public Text scoreText;       // Reference to the UI Text element
+    public AudioClip hitSound;   // Sound to play on hit
+
+    private static int currentScore = 0;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (scoreText != null)
+            UpdateScoreUI();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball")) // Ensure the object is the pinball
+        {
+            currentScore += scoreValue;
+
+            Debug.Log("BALL HIT");
+
+            ScoreManager.Instance.AddScore(100);
+
+            if (scoreText != null)
+                UpdateScoreUI();
+
+            if (audioSource != null && hitSound != null)
+                audioSource.PlayOneShot(hitSound);
+        }
+    }
+
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + currentScore;
+    }
+}
